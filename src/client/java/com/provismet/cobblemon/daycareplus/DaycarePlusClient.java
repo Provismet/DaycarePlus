7966bedc04b619b37ebbe351d8cf708250f07a96
Provismet <17149901+Provismet@users.noreply.github.com>
2026-01-05 18:47:22 +0000

    Enforce resourcepack on the client

--- src/client/java/com/provismet/cobblemon/daycareplus/DaycarePlusClient.java
+++ src/client/java/com/provismet/cobblemon/daycareplus/DaycarePlusClient.java
@@ -2,6 +2,7 @@ package com.provismet.cobblemon.daycareplus;
 
 import com.provismet.cobblemon.daycareplus.config.ClientOptions;
 import com.provismet.cobblemon.daycareplus.networking.callback.PacketCallbacksS2C;
+import eu.pb4.polymer.resourcepack.api.PolymerResourcePackUtils;
 import net.fabricmc.api.ClientModInitializer;
 
 public class DaycarePlusClient implements ClientModInitializer {
@@ -9,5 +10,6 @@ public class DaycarePlusClient implements ClientModInitializer {
     public void onInitializeClient () {
         ClientOptions.load();
         PacketCallbacksS2C.register();
+        PolymerResourcePackUtils.markAsRequired();
     }
 }
